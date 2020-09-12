ca9f8446f0447affb20c40153ba86ead76cd07d1
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-09-12 10:17:46 +0200

    Add buttons in index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function playGame (playerInput){
 
+  function clearMessages ();
+
   let randomNumber = Math.floor (Math.random () * 3 + 1);
 
   console.log ('Wylosowana liczba to: ' + randomNumber);
